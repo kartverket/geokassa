8a3577a392c64969c5a9379b9ec82cd65dc2fe26
Sveinung Himle <Sveinung.Himle@kartverket.no>
2021-02-10 11:49:42 +0100

    Core test app

---
+++ MyCoreApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml;
+
+namespace MyCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dette er mitt aller fyrste Coreprosjekt!");
+        }
+    }
+}
