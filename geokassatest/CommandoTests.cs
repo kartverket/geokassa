4cbb4c7bf749a3529dd17577667f0755b8159f01
Sveinung Himle <Sveinung.Himle@kartverket.no>
2021-04-09 12:22:02 +0200

    CommandoTests added

---
+++ geokassatest/CommandoTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; 
+using geokassa;
+using gridfiles;
+using Xunit;
+
+namespace geokassatests
+{
+    public class CommandoTests
+    {
+    }
+}
