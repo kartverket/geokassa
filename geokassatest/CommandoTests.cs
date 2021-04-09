9e631ccefe8c216e7fa2a07ac59b27e00e895064
Sveinung Himle <Sveinung.Himle@kartverket.no>
2021-04-09 12:29:57 +0200

    New dll's

--- geokassatest/CommandoTests.cs
+++ geokassatest/CommandoTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.CommandLine;
 using System.Text; 
 using geokassa;
 using gridfiles;
@@ -9,5 +10,13 @@ namespace geokassatests
 {
     public class CommandoTests
     {
+        [Fact]
+        public void JsonTinTest()
+        {
+            var rootCommand =  new RootCommand()
+            {
+                new JsonTinCommand("jsontin", "Makes triangulated TIN from point clouds")
+            };
+        }
     }
 }
